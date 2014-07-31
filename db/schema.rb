c13c8326ca50b39f111ca6b37a4d47607cdf2577
tony15501 <your.marco.antonio.medina81@gmail.com>
2014-07-31 18:20:51 -0500

    finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140728152627) do
+ActiveRecord::Schema.define(version: 20140731161642) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -52,5 +52,15 @@ ActiveRecord::Schema.define(version: 20140728152627) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
